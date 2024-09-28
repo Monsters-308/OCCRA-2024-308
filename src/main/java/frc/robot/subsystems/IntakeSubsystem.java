0d20d44576500e62aa59bb9a14f9e5ea2e1fcfc1
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2024-09-28 17:41:25 -0400

    Saturday #2 started the intake subsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,14 +4,36 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
 
-  /** Creates a new IntakeSubsystem. */
-  public IntakeSubsystem() {
+  // getting the motor of the intake. :3
+  CANSparkMax IntakeMotor = new CANSparkMax(IntakeConstants.kIntakeMotorCANID, MotorType.kBrushless);
+
+  public IntakeSubsystem(){
+    IntakeMotor.restoreFactoryDefaults(); // <-- restores to default
+    IntakeMotor.setSmartCurrentLimit(IntakeConstants.kSmartCurrentLimitIntake);// <- limit the power usage
+    IntakeMotor.setIdleMode(IntakeConstants.kIntakeMotorIdleMode);
+    IntakeMotor.setInverted(IntakeConstants.kIntakeMotorInverted);
+    IntakeMotor.burnFlash();
+  }
+
+
+  public void IntakeSpin(){
+    IntakeMotor.set(0.5);// <-- idk what speed and if we want a const speed.
+  }
+
+  public void IntakeUnspin(){
+    IntakeMotor.set(0);
 
   }
+
 
   @Override
   public void periodic() {
